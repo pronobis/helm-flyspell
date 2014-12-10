6db5e16965fd11b6dba61cf400831d026409cb2c
Andrzej Pronobis <a.pronobis@gmail.com>
2014-12-10 13:23:51 -0800

    Added documentation.

--- helm-flyspell.el
+++ helm-flyspell.el
@@ -16,6 +16,18 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;; To use, just put your cursor on or after the misspelled word and
+;; run helm-flyspell-correct. You can of course bind it to a key as
+;; well by adding this to your `~/.emacs` file:
+;; (define-key flyspell-mode-map (kbd "C-;") 'helm-flyspell-correct)
+;;
+;; When invoked, it will show the list of corrections suggested by
+;; Flyspell and options to save the word in your personal dictionary
+;; or accept it in the buffer or the session. If a pattern is typed,
+;; it will be used to filter the corrections. It can also be directly
+;; saved to the dictionary, even if it is different from the initial
+;; word. The new typed word will also replace the word typed in the
+;; buffer.
 
 ;;; Code:
 
