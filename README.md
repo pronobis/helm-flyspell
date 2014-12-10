b254ea4530943353d6e0e00e459726024c6a2cdf
Andrzej Pronobis <a.pronobis@gmail.com>
2014-12-09 21:19:30 -0800

    Updated documentation.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 helm-flyspell
 =============
 
-Helm extension for correcting words with Flyspell
+Helm extension for correcting words with Flyspell.
+
+To use, just put your cursor on or after the misspelled word and run `helm-flyspell-correct`. You can of course bind it to a key as well by adding this to your `~/.emacs` file:
+```
+(define-key flyspell-mode-map (kbd "C-;") 'helm-flyspell-correct)
+```
+
+Thanks
+------
+
+Thanks go to [Michael Markert](https://github.com/cofi) for the inspiration and his original [helm-flyspell-correct](https://gist.github.com/cofi/3013327) code.
--- helm-flyspell.el
+++ helm-flyspell.el
@@ -1,5 +1,20 @@
 ;;; helm-flyspell.el -- Helm extension for correcting words with flyspell
 
+;; Copyright (C) 2014 Andrzej Pronobis <a.pronobis@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;;; Code:
@@ -58,8 +73,6 @@
         :prompt "Correction: "))
 
 
-;; Based on the original helm-flyspell-correct found here:
-;; https://github.com/cofi/dotfiles/blob/master/emacs.d/config/cofi-helm.el
 (defun helm-flyspell-correct ()
   "Use helm for flyspell correction.
 Adapted from `flyspell-correct-word-before-point'."
